19ed4cd49461466633745deb9d63e44f946281bb
Yury Polek <yury.polek@gmail.com>
2014-06-24 23:53:55 +0300

    Persisting server list in SharedPreferences

--- app/src/main/java/net/yupol/transmissionremote/app/TransmissionRemote.java
+++ app/src/main/java/net/yupol/transmissionremote/app/TransmissionRemote.java
@@ -2,13 +2,27 @@ package net.yupol.transmissionremote.app;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.google.common.collect.FluentIterable;
 
 import net.yupol.transmissionremote.app.server.Server;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class TransmissionRemote extends Application {
+
+    private static final String SHARED_PREFS_NAME = "transmission_remote_shared_prefs";
+    private static final String KEY_SERVERS = "key_servers";
 
     private List<Server> servers = new LinkedList<>();
 
@@ -16,7 +30,18 @@ public class TransmissionRemote extends Application {
     public void onCreate() {
         super.onCreate();
 
-        // TODO: read persisted servers
+        SharedPreferences sp = getSharedPreferences(SHARED_PREFS_NAME, MODE_PRIVATE);
+        Set<String> serversInJson = sp.getStringSet(KEY_SERVERS, Collections.<String>emptySet());
+        servers.addAll(FluentIterable.from(serversInJson).transform(new Function<String, Server>() {
+            @Override
+            public Server apply(String serverInJson) {
+                try {
+                    return Server.fromJson(new JSONObject(serverInJson));
+                } catch (JSONException e) {
+                    return null;
+                }
+            }
+        }).filter(Predicates.notNull()).toList());
     }
 
     public List<Server> getServers() {
@@ -25,12 +50,26 @@ public class TransmissionRemote extends Application {
 
     public void addServer(Server server) {
         servers.add(server);
-        // TODO: persist server
+        persistServerList();
     }
 
     public void removeServer(Server server) {
         servers.remove(server);
-        // TODO: remove server from persisted storage
+        persistServerList();
+    }
+
+    private void persistServerList() {
+        Set<String> serversInJson = FluentIterable.from(servers).transform(new Function<Server, String>() {
+            @Override
+            public String apply(Server server) {
+                return server.toJson().toString();
+            }
+        }).toSet();
+
+        SharedPreferences sp = getSharedPreferences(SHARED_PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putStringSet(KEY_SERVERS, serversInJson);
+        editor.commit();
     }
 
     public static TransmissionRemote getApplication(Context context) {
--- app/src/main/java/net/yupol/transmissionremote/app/server/Server.java
+++ app/src/main/java/net/yupol/transmissionremote/app/server/Server.java
@@ -2,10 +2,16 @@ package net.yupol.transmissionremote.app.server;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import static com.google.common.base.Preconditions.*;
 
 public class Server implements Parcelable {
+
+    public static final String TAG = Server.class.getSimpleName();
 
     private String name;
     private String host;
@@ -52,6 +58,42 @@ public class Server implements Parcelable {
 
     public String getPassword() {
         return password;
+    }
+
+    public JSONObject toJson() {
+        JSONObject obj = new JSONObject();
+
+        try {
+            obj.put("name", name);
+            obj.put("host", host);
+            obj.put("port", port);
+            obj.put("useAuthentication", useAuthentication);
+            obj.putOpt("userName", userName);
+            obj.putOpt("password", password);
+        } catch (JSONException e) {
+            Log.e(TAG, "Failed to create JSON object from Server object: " + this, e);
+        }
+
+        return obj;
+    }
+
+    public static Server fromJson(JSONObject obj) {
+        try {
+            String name = obj.getString("name");
+            String host = obj.getString("host");
+            int port = obj.getInt("port");
+            boolean useAuthentication = obj.getBoolean("useAuthentication");
+            if (useAuthentication) {
+                String userName = obj.optString("userName", null);
+                String password = obj.optString("password", null);
+                return new Server(name, host, port, userName, password);
+            } else {
+                return new Server(name, host, port);
+            }
+        } catch (JSONException e) {
+            Log.e(TAG, "Failed to create Server object from JSON object: " + obj, e);
+            return null;
+        }
     }
 
     @Override
