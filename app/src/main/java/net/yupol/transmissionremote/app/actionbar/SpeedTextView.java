b6a7a325f6bf7ac9f0173d799b34161ce37c1b52
Yury Polek <yury.polek@gmail.com>
2015-05-31 14:54:39 +0300

    using images instead of unicode characters for speed text tool items

--- app/src/main/java/net/yupol/transmissionremote/app/actionbar/SpeedTextView.java
+++ app/src/main/java/net/yupol/transmissionremote/app/actionbar/SpeedTextView.java
@@ -2,36 +2,43 @@ package net.yupol.transmissionremote.app.actionbar;
 
 import android.content.Context;
 import android.graphics.Typeface;
-import android.text.Html;
+import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
 import android.widget.TextView;
 
 import com.google.common.base.Strings;
 
+import net.yupol.transmissionremote.app.R;
 import net.yupol.transmissionremote.app.utils.MetricsUtils;
 import net.yupol.transmissionremote.app.utils.SizeUtils;
 
 public abstract class SpeedTextView extends TextView {
 
     private static final int PADDING_LEFT = 5; // dp
-
-    private Direction direction = Direction.NONE;
+    private static final int PADDING_IMAGE = 8; // dp
 
     public SpeedTextView(Context context, Direction direction) {
         super(context);
-        this.direction = direction;
         int horPadding = (int) MetricsUtils.dp2px(context, PADDING_LEFT);
         setPadding(horPadding, 0, horPadding, 0);
+
+        setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimensionPixelSize(R.dimen.speed_text_size));
         setTypeface(null, Typeface.BOLD);
+
         setSpeed(0);
+
+        setCompoundDrawablePadding((int) MetricsUtils.dp2px(context, PADDING_IMAGE));
+        Drawable drawable = getResources().getDrawable(direction.iconId);
+        if (drawable != null) {
+            measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED);
+            int size = 2 * getMeasuredHeight() / 3;
+            drawable.setBounds(0, 0, size, size);
+        }
+        setCompoundDrawables(drawable, null, null, null);
     }
 
     public void setSpeed(long speedInBytesPerSec) {
-        String speedText = speedText(speedInBytesPerSec);
-
-        if (direction != Direction.NONE) {
-            speedText = direction.symbol + " " + speedText;
-        }
-        setText(Html.fromHtml(speedText));
+        setText(speedText(speedInBytesPerSec));
     }
 
     private String speedText(long bytes) {
@@ -39,15 +46,14 @@ public abstract class SpeedTextView extends TextView {
     }
 
     private enum Direction {
-        DOWNLOAD("<font color=#009933>\u2b07</font>"),
-        UPLOAD("<font color=#ff0000>\u2b06</font>"),
-        NONE("");
+        DOWNLOAD(R.drawable.download_icon),
+        UPLOAD(R.drawable.upload_icon);
 
 
-        private String symbol;
+        private int iconId;
 
-        Direction(String symbol) {
-            this.symbol = symbol;
+        Direction(int iconId) {
+            this.iconId = iconId;
         }
     }
 
