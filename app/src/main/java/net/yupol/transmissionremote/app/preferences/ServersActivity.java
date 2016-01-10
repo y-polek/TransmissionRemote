9b8012980efa112eafd6f213c4e44a166bf05c84
Yury Polek <yury.polek@gmail.com>
2016-01-10 23:25:35 +0200

    Updated Android SDK and dependencies

--- app/src/main/java/net/yupol/transmissionremote/app/preferences/ServersActivity.java
+++ app/src/main/java/net/yupol/transmissionremote/app/preferences/ServersActivity.java
@@ -7,6 +7,7 @@ import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -34,7 +35,10 @@ public class ServersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.servers_activity_layout);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
         if (savedInstanceState != null) {
             return;
