543f00d58997b5da798e6091975213f01c47827e
Yury Polek <yury.polek@gmail.com>
2016-07-10 15:17:25 +0300

    add simple animation of directory RecyclerView

--- app/src/main/java/net/yupol/transmissionremote/app/torrentdetails/DirectoryAdapter.java
+++ app/src/main/java/net/yupol/transmissionremote/app/torrentdetails/DirectoryAdapter.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 
@@ -23,6 +25,7 @@ public class DirectoryAdapter extends RecyclerView.Adapter<DirectoryAdapter.View
     private FileStat[] fileStats;
     private Dir dir = Dir.emptyDir();
     private OnItemSelectedListener listener;
+    private int lastPosition = -1;
 
     public DirectoryAdapter(File[] files, FileStat[] fileStats, OnItemSelectedListener listener) {
         this.files = files;
@@ -58,6 +61,13 @@ public class DirectoryAdapter extends RecyclerView.Adapter<DirectoryAdapter.View
         }
         holder.binding.setFileStats(fileStats);
         holder.binding.executePendingBindings();
+        setAnimation(holder.binding.getRoot(), position);
+    }
+
+    private void setAnimation(View viewToAnimate, int position) {
+        Animation animation = AnimationUtils.loadAnimation(viewToAnimate.getContext(), R.anim.enter_from_right);
+        viewToAnimate.startAnimation(animation);
+        lastPosition = position;
     }
 
     @Override
