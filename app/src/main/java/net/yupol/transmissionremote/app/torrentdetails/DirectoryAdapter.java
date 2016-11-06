f763e251533b21adef04298bc960e9ab07ee9653
Yury Polek <yury.polek@gmail.com>
2016-11-06 19:04:41 +0200

    use three state checkbox for file selection

--- app/src/main/java/net/yupol/transmissionremote/app/torrentdetails/DirectoryAdapter.java
+++ app/src/main/java/net/yupol/transmissionremote/app/torrentdetails/DirectoryAdapter.java
@@ -3,14 +3,14 @@ package net.yupol.transmissionremote.app.torrentdetails;
 import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
 
+import com.buildware.widget.indeterm.IndeterminateCheckBox;
 import com.mikepenz.fontawesome_typeface_library.FontAwesome;
 import com.mikepenz.iconics.IconicsDrawable;
 
@@ -28,12 +28,12 @@ public class DirectoryAdapter extends RecyclerView.Adapter<DirectoryAdapter.View
     private Context context;
     private File[] files;
     private FileStat[] fileStats;
-    private Dir dir;
+    private Dir currentDir;
     private OnItemSelectedListener listener;
 
     public DirectoryAdapter(Context context, Dir dir, File[] files, FileStat[] fileStats, OnItemSelectedListener listener) {
         this.context = context;
-        this.dir = dir;
+        currentDir = dir;
         this.files = files;
         this.fileStats = fileStats;
         this.listener = listener;
@@ -55,39 +55,81 @@ public class DirectoryAdapter extends RecyclerView.Adapter<DirectoryAdapter.View
             case R.id.view_type_directory:
                 Dir dir = getDir(position);
                 holder.binding.setDir(dir);
+
+                holder.binding.checkbox.setState(isDirectoryChecked(dir));
+                holder.binding.checkbox.setEnabled(!isDirectoryCompleted(dir));
+
                 icon = new IconicsDrawable(context, FontAwesome.Icon.faw_folder_o)
                         .color(ContextCompat.getColor(context, R.color.text_color_primary));
                 break;
             case R.id.view_type_file:
                 File file = getFile(position);
                 holder.binding.setFile(file);
-                holder.binding.setFileStat(getFileStat(position));
+
+                holder.binding.checkbox.setChecked(isFileChecked(position));
+                holder.binding.checkbox.setEnabled(!isFileCompleted(position));
+
                 icon = new IconicsDrawable(context, FileType.iconFromName(file.getName()))
                         .color(ContextCompat.getColor(context, R.color.text_color_secondary));
                 break;
         }
-        holder.binding.setFileStats(fileStats);
-        holder.binding.executePendingBindings();
         holder.binding.icon.setImageDrawable(icon);
+        holder.binding.executePendingBindings();
+    }
+
+    private boolean isFileChecked(int position) {
+        return isFileChecked(getFile(position), getFileStat(position));
+    }
+
+    private boolean isFileChecked(File file, FileStat fileStat) {
+        return fileStat.isWanted() || isFileCompleted(file);
+    }
+
+    private boolean isFileCompleted(int position) {
+        return isFileCompleted(getFile(position));
+    }
+
+    private boolean isFileCompleted(File file) {
+        return file.getBytesCompleted() >= file.getLength();
+    }
+
+    @Nullable
+    private Boolean isDirectoryChecked(Dir dir) {
+        boolean hasCheckedFiles = false;
+        boolean hasUncheckedFiles = false;
+        for (Integer fileIndex : dir.getFileIndices()) {
+            boolean isFileChecked = isFileChecked(files[fileIndex], fileStats[fileIndex]);
+            hasCheckedFiles |= isFileChecked;
+            hasUncheckedFiles |= !isFileChecked;
+            if (hasCheckedFiles && hasUncheckedFiles) return null;
+        }
+        return hasCheckedFiles;
+    }
+
+    private boolean isDirectoryCompleted(Dir dir) {
+        for (Integer fileIndex : dir.getFileIndices()) {
+            if (!isFileCompleted(files[fileIndex])) return false;
+        }
+        return true;
     }
 
     @Override
     public int getItemCount() {
-        return dir.getDirs().size() + dir.getFileIndices().size();
+        return currentDir.getDirs().size() + currentDir.getFileIndices().size();
     }
 
     @Override
     public int getItemViewType(int position) {
-        return position >= dir.getDirs().size() ? R.id.view_type_file : R.id.view_type_directory;
+        return position >= currentDir.getDirs().size() ? R.id.view_type_file : R.id.view_type_directory;
     }
 
     @SuppressWarnings("unchecked")
     private <T> T getItem(int position) {
-        List<Dir> dirs = dir.getDirs();
+        List<Dir> dirs = currentDir.getDirs();
         if (position < dirs.size()) {
             return (T) dirs.get(position);
         } else {
-            Integer fileIndex = dir.getFileIndices().get(position - dirs.size());
+            Integer fileIndex = currentDir.getFileIndices().get(position - dirs.size());
             return (T) fileIndex;
         }
     }
@@ -110,7 +152,7 @@ public class DirectoryAdapter extends RecyclerView.Adapter<DirectoryAdapter.View
 
         private FileItemBinding binding;
 
-        public ViewHolder(FileItemBinding binding, final OnItemSelectedListener listener) {
+        public ViewHolder(final FileItemBinding binding, final OnItemSelectedListener listener) {
             super(binding.getRoot());
             this.binding = binding;
 
@@ -122,19 +164,21 @@ public class DirectoryAdapter extends RecyclerView.Adapter<DirectoryAdapter.View
                     if (getItemViewType() == R.id.view_type_directory) {
                         listener.onDirectorySelected(getAdapterPosition());
                     } else {
-                        listener.onFileSelected(getAdapterPosition() - dir.getDirs().size());
+                        if (binding.checkbox.isEnabled()) {
+                            binding.checkbox.setChecked(!binding.checkbox.isChecked());
+                        }
                     }
 
                 }
             });
-            CheckBox checkBox = (CheckBox) binding.getRoot().findViewById(R.id.checkbox);
-            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            binding.checkbox.setOnStateChangedListener(new IndeterminateCheckBox.OnStateChangedListener() {
                 @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                public void onStateChanged(IndeterminateCheckBox buttonView, @Nullable Boolean isChecked) {
                     if (getItemViewType() == R.id.view_type_directory) {
-                        listener.onDirectoryChecked(getAdapterPosition(), isChecked);
+                        if (isChecked != null) listener.onDirectoryChecked(getAdapterPosition(), isChecked);
                     } else {
                         Integer fileIndex = getItem(getAdapterPosition());
+                        assert isChecked != null;
                         listener.onFileChecked(fileIndex, isChecked);
                     }
                 }
@@ -144,7 +188,6 @@ public class DirectoryAdapter extends RecyclerView.Adapter<DirectoryAdapter.View
 
     public interface OnItemSelectedListener {
         void onDirectorySelected(int position);
-        void onFileSelected(int position);
         void onDirectoryChecked(int position, boolean isChecked);
         void onFileChecked(int fileIndex, boolean isChecked);
     }
