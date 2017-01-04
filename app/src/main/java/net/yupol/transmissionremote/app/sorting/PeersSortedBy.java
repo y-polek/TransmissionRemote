68cced56c0ba073dcf6bd8fae5918cd0b4298f3a
Yury Polek <yury.polek@gmail.com>
2017-01-04 23:33:46 +0200

    add Peers sorting model

---
+++ app/src/main/java/net/yupol/transmissionremote/app/sorting/PeersSortedBy.java
@@ -0,0 +1,60 @@
+package net.yupol.transmissionremote.app.sorting;
+
+import android.support.annotation.StringRes;
+
+import com.google.common.base.Strings;
+
+import net.yupol.transmissionremote.app.R;
+import net.yupol.transmissionremote.app.model.json.Peer;
+
+import java.util.Comparator;
+
+public enum PeersSortedBy {
+
+    ADDRESS(R.string.peers_sort_by_address, new Comparator<Peer>() {
+        @Override
+        public int compare(Peer p1, Peer p2) {
+            String address1 = Strings.nullToEmpty(p1.address);
+            String address2 = Strings.nullToEmpty(p2.address);
+            return address1.compareToIgnoreCase(address2);
+        }
+    }),
+
+    CLIENT(R.string.peers_sort_by_client, new Comparator<Peer>() {
+        @Override
+        public int compare(Peer p1, Peer p2) {
+            String client1 = Strings.nullToEmpty(p1.clientName);
+            String client2 = Strings.nullToEmpty(p2.clientName);
+            return client1.compareToIgnoreCase(client2);
+        }
+    }),
+
+    PROGRESS(R.string.peers_sort_by_progress, new Comparator<Peer>() {
+        @Override
+        public int compare(Peer p1, Peer p2) {
+            return Double.compare(p1.progress, p2.progress);
+        }
+    }),
+
+    DOWNLOADED_RATE(R.string.peers_sort_by_download_rate, new Comparator<Peer>() {
+        @Override
+        public int compare(Peer p1, Peer p2) {
+            return Long.signum(p2.rateToClient - p1.rateToClient);
+        }
+    }),
+
+    UPLOAD_RATE(R.string.peers_sort_by_upload_rate, new Comparator<Peer>() {
+        @Override
+        public int compare(Peer p1, Peer p2) {
+            return Long.signum(p2.rateToPeer - p1.rateToPeer);
+        }
+    });
+
+    @StringRes public final int nameResId;
+    public final Comparator<Peer> comparator;
+
+    PeersSortedBy(@StringRes int nameResId, Comparator<Peer> comparator) {
+        this.nameResId = nameResId;
+        this.comparator = comparator;
+    }
+}
