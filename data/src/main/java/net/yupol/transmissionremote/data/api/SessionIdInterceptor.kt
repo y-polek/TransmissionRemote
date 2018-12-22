328f48f492a3679a253c8e9901808421b3e49a9e
Yury Polek <yury.polek@gmail.com>
2018-12-22 12:07:16 +0200

    add caching of Session ID in SessionIdInterceptor

--- data/src/main/java/net/yupol/transmissionremote/data/api/SessionIdInterceptor.kt
+++ data/src/main/java/net/yupol/transmissionremote/data/api/SessionIdInterceptor.kt
@@ -1,6 +1,7 @@
 package net.yupol.transmissionremote.data.api
 
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 import javax.inject.Inject
 
@@ -10,16 +11,25 @@ class SessionIdInterceptor @Inject constructor(): Interceptor {
         const val SESSION_ID_HEADER = "X-Transmission-Session-Id"
     }
 
+    private var sessionId: String? = null
+
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
+        val request = chain.request().withSessionId()
 
         val response = chain.proceed(request)
-        if (response.code() != 409) return response
 
-        val sessionId = response.header(SESSION_ID_HEADER) ?: return response
+        if (response.code() == 409) {
+            sessionId = response.header(SESSION_ID_HEADER)
+            if (sessionId != null) {
+                return chain.proceed(request.withSessionId())
+            }
+        }
 
-        return chain.proceed(request.newBuilder()
-                .header(SESSION_ID_HEADER, sessionId)
-                .build())
+        return response
+    }
+
+    private fun Request.withSessionId(): Request {
+        sessionId ?: return this
+        return newBuilder().header(SESSION_ID_HEADER, sessionId.orEmpty()).build()
     }
 }
