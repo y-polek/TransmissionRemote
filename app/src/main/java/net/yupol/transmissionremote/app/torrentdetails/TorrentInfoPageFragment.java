2246f5dda40eadf8c6f2e668779bf75c314ae2cf
Yury Polek <yury.polek@gmail.com>
2020-05-31 12:06:21 +0300

    copy magnet link by long click

--- app/src/main/java/net/yupol/transmissionremote/app/torrentdetails/TorrentInfoPageFragment.java
+++ app/src/main/java/net/yupol/transmissionremote/app/torrentdetails/TorrentInfoPageFragment.java
@@ -1,5 +1,8 @@
 package net.yupol.transmissionremote.app.torrentdetails;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -7,15 +10,16 @@ import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import net.yupol.transmissionremote.app.R;
 import net.yupol.transmissionremote.app.databinding.TorrentDetailsInfoPageFragmentBinding;
 import net.yupol.transmissionremote.app.model.json.Torrent;
 import net.yupol.transmissionremote.app.model.json.TorrentInfo;
 
-public class TorrentInfoPageFragment extends BasePageFragment {
+import static com.google.common.base.Strings.nullToEmpty;
 
-    private static final String TAG = TorrentInfoPageFragment.class.getSimpleName();
+public class TorrentInfoPageFragment extends BasePageFragment {
 
     private TorrentDetailsInfoPageFragmentBinding binding;
 
@@ -25,6 +29,13 @@ public class TorrentInfoPageFragment extends BasePageFragment {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.torrent_details_info_page_fragment, container, false);
         binding.magnetText.setMovementMethod(LinkMovementMethod.getInstance());
+        binding.magnetText.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                copyMagnetLinkToClipboard();
+                return true;
+            }
+        });
         binding.setTorrent(getTorrent());
         binding.setTorrentInfo(getTorrentInfo());
 
@@ -44,6 +55,19 @@ public class TorrentInfoPageFragment extends BasePageFragment {
         super.setTorrent(torrent);
         if (binding != null) {
             binding.setTorrent(torrent);
+        }
+    }
+
+    private void copyMagnetLinkToClipboard() {
+        Torrent torrent = getTorrent();
+        TorrentInfo torrentInfo = getTorrentInfo();
+        if (torrent == null || torrentInfo == null) return;
+
+        ClipboardManager clipboard = (ClipboardManager) requireActivity().getSystemService(Context.CLIPBOARD_SERVICE);
+        if (clipboard != null) {
+            ClipData clip = ClipData.newPlainText(nullToEmpty(torrent.getName()), nullToEmpty(torrentInfo.getMagnetLink()));
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(getContext(), R.string.copied, Toast.LENGTH_SHORT).show();
         }
     }
 }
