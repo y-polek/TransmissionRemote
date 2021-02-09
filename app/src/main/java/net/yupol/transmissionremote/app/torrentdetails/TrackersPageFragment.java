cefe64275cf396695707e0ad9ed795d03c949b27
Yaroslav Kazarin <Kazarin-Yaroslav@yandex.ru>
2021-02-10 01:23:37 +0300

    Added sorting trackers by tiers, seeders, leechers and downloaded.

--- app/src/main/java/net/yupol/transmissionremote/app/torrentdetails/TrackersPageFragment.java
+++ app/src/main/java/net/yupol/transmissionremote/app/torrentdetails/TrackersPageFragment.java
@@ -5,8 +5,10 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -14,12 +16,14 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.ListPopupWindow;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Toast;
 
 import com.mikepenz.community_material_typeface_library.CommunityMaterial;
@@ -30,6 +34,8 @@ import net.yupol.transmissionremote.app.R;
 import net.yupol.transmissionremote.app.databinding.TorrentDetailsTrackersPageFragmentBinding;
 import net.yupol.transmissionremote.app.model.json.TorrentInfo;
 import net.yupol.transmissionremote.app.model.json.TrackerStats;
+import net.yupol.transmissionremote.app.sorting.SortOrder;
+import net.yupol.transmissionremote.app.sorting.TrackersSortedBy;
 import net.yupol.transmissionremote.app.transport.BaseSpiceActivity;
 import net.yupol.transmissionremote.app.transport.TransportManager;
 import net.yupol.transmissionremote.app.transport.request.TrackerAddRequest;
@@ -37,6 +43,8 @@ import net.yupol.transmissionremote.app.transport.request.TrackerRemoveRequest;
 import net.yupol.transmissionremote.app.transport.request.TrackerReplaceRequest;
 import net.yupol.transmissionremote.app.utils.DividerItemDecoration;
 import net.yupol.transmissionremote.app.utils.IconUtils;
+import net.yupol.transmissionremote.app.utils.MetricsUtils;
+import net.yupol.transmissionremote.app.utils.Size;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -44,12 +52,15 @@ public class TrackersPageFragment extends BasePageFragment implements TrackersAd
         TrackerUrlDialog.OnTrackerUrlEnteredListener {
 
     private static final String TAG_EDIT_URL_DIALOG = "tag_edit_url_dialog";
+    private static final String KEY_SORT_BY = "key_sort_by";
+    private static final String KEY_SORT_ORDER = "key_sort_order";
 
     private TrackersAdapter adapter = new TrackersAdapter(this);
     private TorrentDetailsTrackersPageFragmentBinding binding;
     private boolean viewCreated;
     private ClipboardManager clipboardManager;
     private TransportManager transportManager;
+    private SharedPreferences preferences;
 
     @Override
     public void onAttach(Context context) {
@@ -66,6 +77,7 @@ public class TrackersPageFragment extends BasePageFragment implements TrackersAd
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
+        preferences = PreferenceManager.getDefaultSharedPreferences(getContext());
     }
 
     @Override
@@ -91,6 +103,8 @@ public class TrackersPageFragment extends BasePageFragment implements TrackersAd
             binding.swipeRefresh.setRefreshing(true);
         }
 
+        adapter.setSorting(currentSorting(), currentSortOrder());
+
         viewCreated = true;
 
         return binding.getRoot();
@@ -106,6 +120,7 @@ public class TrackersPageFragment extends BasePageFragment implements TrackersAd
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.torrent_trackers_menu, menu);
         IconUtils.setMenuIcon(getContext(), menu, R.id.action_add, CommunityMaterial.Icon2.cmd_plus);
+        IconUtils.setMenuIcon(getContext(), menu, R.id.action_sort_trackers, CommunityMaterial.Icon2.cmd_sort_variant);
     }
 
     @Override
@@ -114,8 +129,69 @@ public class TrackersPageFragment extends BasePageFragment implements TrackersAd
             case R.id.action_add:
                 showEditTrackerUrlDialog(null);
                 return true;
+            case R.id.action_sort_trackers:
+                showSortingList();
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showSortingList() {
+        ListPopupWindow popup = new ListPopupWindow(requireContext());
+        popup.setModal(true);
+
+        final TrackersSortingListAdapter sortingAdapter = new TrackersSortingListAdapter();
+        final TrackersSortedBy sortedBy = currentSorting();
+        SortOrder sortOrder = currentSortOrder();
+        sortingAdapter.setCurrentSorting(sortedBy, sortOrder);
+        popup.setAdapter(sortingAdapter);
+        popup.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TrackersSortedBy sorting = sortingAdapter.getItem(position);
+                SortOrder order = SortOrder.ASCENDING;
+                if (sorting == currentSorting()) {
+                    order = currentSortOrder().reversed();
+                }
+                setCurrentSorting(sorting, order);
+                sortingAdapter.setCurrentSorting(sorting, order);
+                adapter.setSorting(sorting, order);
+            }
+        });
+
+        popup.setAnchorView(binding.getRoot());
+        Size popupSize = MetricsUtils.measurePopupSize(getContext(), sortingAdapter);
+        popup.setContentWidth(popupSize.width);
+        popup.setHeight(popupSize.height);
+        popup.setHorizontalOffset(binding.getRoot().getWidth() - popupSize.width
+                - getResources().getDimensionPixelOffset(R.dimen.sort_popup_offset_horizontal));
+        popup.setVerticalOffset(-getResources().getDimensionPixelOffset(R.dimen.sort_popup_offset_vertical));
+        popup.show();
+    }
+
+    private TrackersSortedBy currentSorting() {
+        try {
+            return TrackersSortedBy.valueOf(preferences.getString(KEY_SORT_BY, TrackersSortedBy.TIERS.toString()));
+        } catch (IllegalArgumentException e) {
+            // Unknown sorting, return default
+            return TrackersSortedBy.TIERS;
+        }
+    }
+
+    private void setCurrentSorting(TrackersSortedBy sorting, SortOrder sortOrder) {
+        preferences.edit()
+                .putString(KEY_SORT_BY, sorting.toString())
+                .putString(KEY_SORT_ORDER, sortOrder.toString())
+                .apply();
+    }
+
+    private SortOrder currentSortOrder() {
+        try {
+            return SortOrder.valueOf(preferences.getString(KEY_SORT_ORDER, SortOrder.ASCENDING.toString()));
+        } catch (IllegalArgumentException e) {
+            // Unknown order, return default
+            return SortOrder.ASCENDING;
+        }
     }
 
     @Override
